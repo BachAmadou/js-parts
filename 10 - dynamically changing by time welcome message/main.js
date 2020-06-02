51e001663956881cf47c7e957681aab543987c9f
Bach <bafosth@yahoo.fr>
2020-06-02 15:20:35 -0400

    Added script to change the background color of the message

--- 10 - dynamically changing by time welcome message/main.js
+++ 10 - dynamically changing by time welcome message/main.js
@@ -1,5 +1,6 @@
 /*
-Change the message dynamicallu depending on the time.
+---Change the message dynamically depending on the time.
+---Change the message background color
 */
 
 const messageBtn = document.querySelector("button");
@@ -15,12 +16,16 @@ function showMessage() {
 
     if( currentHour > 17) {
         message = "its evening";
+        output.style.backgroundColor = "black";
     } else if( currentHour > 12) {
         message = "its afternoon";
+        output.style.backgroundColor = "blue";
     } else if( currentHour > 0) {
         message = "its morning";
+        output.style.backgroundColor = "yellow";
     } else {
         message = "Something is wrong";
+        output.style.backgroundColor = "red";
     }
     output.innerHTML = "<h1>" +message+ "</h1";
 }
