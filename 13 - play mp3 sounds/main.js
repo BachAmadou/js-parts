aec5019c95827b68f6b609495e58cd5a804fbcb4
Bach <bafosth@yahoo.fr>
2020-06-06 13:50:55 -0400

    Refactored code for: play mp3 sounds and added project 14: image popup window

--- 13 - play mp3 sounds/main.js
+++ 13 - play mp3 sounds/main.js
@@ -2,61 +2,30 @@
 This is the lesson where we're going to show you how you can play sounds MP 3 files using JavaScript.
 */
 
-const musics = document.querySelectorAll(".music");
-console.log(musics);
+myMusic = ["wutang", "Gza", "kizumba"];
+//console.log(myMusic);
 
-for( let i = 0; i < musics.length; i++) {
-    musics[i].addEventListener("click", function() {
-        let musicType = this.innerText;
-        let lowerCaseMusic = musicType.toLowerCase();
-        //console.log(lowerCaseMusic);
-        makeSound(lowerCaseMusic);
-        addStyle(lowerCaseMusic);
-    });
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    myMusic.forEach(function(item) {
+        let div = document.createElement("div");
+        div.setAttribute("class", "music" + item);
+        div.innerText = item.toUpperCase();
+        div.addEventListener("click", function() {
+          playMusic(item);
+        });
+        document.body.appendChild(div);
+      });
 }
 
-function addStyle(name) {
+function playMusic(name) {
     let activeEl = document.querySelector("." + name);
-    console.log(activeEl);
+    //console.log(activeEl);
+    let sound = new Audio("sounds/" + name + ".mp3");
+    sound.play();
     activeEl.classList.add("active");
     setTimeout(function(){
         activeEl.classList.remove("active");
     }, 200);
 }
-
-function makeSound(name) {
-    console.log(name);
-    switch(name) {
-        case "7th chambers":
-        let sound1 = new Audio("sounds/wutang.mp3");
-        sound1.play();
-        break;
-
-        case "4th chambers":
-        let sound2 = new Audio("sounds/Gza.mp3");
-        sound2.play();
-        break;
-
-        case "kiz":
-        let sound3 = new Audio("sounds/kiz.mp3");
-        sound3.play();
-        break;
-    }
-}
-
-
-
-
-
-
-
-
-// const button = document.querySelector("button");
-// const kiz = new Audio('sounds/kiz.mp3');
-
-// button.addEventListener("click", function() {
-//     let duration = kiz.duration;
-//     let muted = kiz.muted;
-//     kiz.play();
-//     console.log(muted);
-// });
