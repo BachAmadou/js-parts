ce1abd34a06b78fa77a0c4ef2b13727e91e9408d
Bach <bafosth@yahoo.fr>
2020-06-23 08:59:51 -0400

    added more js script

--- 17 - star rating/main.js
+++ 17 - star rating/main.js
@@ -3,12 +3,48 @@ const output = document.querySelector(".output");
 
 for(let i = 0; i < stars.length; i++) {
     stars[i].starValue = (i + 1);
-    console.log(stars[i]);
+    //console.log(stars[i]);
 
-    stars[i].addEventListener("click", starRate); 
-
+    ["mouseover","mouseout","click"].forEach(function(e) {
+        stars[i].addEventListener(e, starRate);
+    });
 }
 
 function starRate(e) {
-    console.log(this.starValue);
+    //console.log(this.starValue);
+    // console.log(e.type);
+    let t = e.type;
+    let starValue = this.starValue;
+
+    stars.forEach(function(ele, indexValue) {
+        //console.log(ele);
+        //console.log(ind);
+        if(t === "click") {
+            if(starValue > 1) {
+                output.innerHTML = "You rated this " + starValue + " stars";
+            }
+        }
+
+        if (t === "click") {
+            if(indexValue < starValue) {
+                ele.classList.add("orange");
+            } else {
+                ele.classList.remove("orange");
+            }
+        }
+
+        if (t === "mouseover") {
+            if(indexValue < starValue) {
+                ele.classList.add("yellow");
+            } else {
+                ele.classList.remove("yellow");
+            }
+        }
+
+        if (t === "mouseout") {
+            if(indexValue < starValue) {
+                ele.classList.remove("yellow");
+            }
+        }
+    });
 }
