7d530842d8715ce7d1eb1f481f79f307f0f63f34
Bach <bafosth@yahoo.fr>
2020-06-26 13:09:22 -0400

    Added dynamic typing project and its html and js files

---
+++ 21 - dynamic typing test/main.js
@@ -0,0 +1,3 @@
+/*
+Just some javascript behind the scenes and the objective here is to input a question into the input field and then get a random response being returned back via javascript and then outputting it on the page.
+*/
