f19de9fccfee2a8fb3986282ce4a303466e172a2
Bach <bafosth@yahoo.fr>
2020-06-26 06:54:19 -0400

    Added random response generator project and its html and js files

---
+++ 20 - random response/main.js
@@ -0,0 +1,4 @@
+/*
+Just some javascript behind the scenes and the objective here is to input a question into the input field and then get a random response being returned back via javascript and then outputting it on the page.
+*/
+
