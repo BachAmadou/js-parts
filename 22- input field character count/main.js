411973e1046e42405ef2a7b20c130d70b7b9fc08
Bach <bafosth@yahoo.fr>
2020-06-27 09:48:21 -0400

    Added input field character count project, and its html and js files

---
+++ 22- input field character count/main.js
@@ -0,0 +1,7 @@
+/*
+we've got an input field that we want to count how many characters are still available within the input field and how many characters are added in there.
+
+And then if there is a limit.
+
+If we only want 10 characters then we're going to output a message whenever we detect that the character count is above what is allocated for the input field.
+*/
