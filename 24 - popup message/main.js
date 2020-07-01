ce3deae622a2da881959ecfa207902c5501d85f6
Bach <bafosth@yahoo.fr>
2020-06-30 22:51:32 -0400

    added project description to the js file

--- 24 - popup message/main.js
+++ 24 - popup message/main.js
@@ -1,3 +1,8 @@
+/*
+Click an element, shows modal popup message with close button.
+Message contents from element attributes.
+*/
+
 const popups = document.querySelectorAll(".popup");
 const pop = document.querySelector(".output");
 const popMessage = document.querySelector(".message");
