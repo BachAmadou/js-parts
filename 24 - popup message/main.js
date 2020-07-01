05f7c7506ddad1975f22306908c2afa7bbaaaa65
Bach <bafosth@yahoo.fr>
2020-06-30 22:45:18 -0400

    added html, css content and js script

---
+++ 24 - popup message/main.js
@@ -0,0 +1,24 @@
+const popups = document.querySelectorAll(".popup");
+const pop = document.querySelector(".output");
+const popMessage = document.querySelector(".message");
+const closeBtn = document.querySelector("button");
+
+for(let i = 0; i < popups.length; i++) {
+    //console.log(popups);
+    popups[i].addEventListener("click", function() {
+        let outputText = this.getAttribute("data-message");
+        //console.log(popupText);
+        message(outputText);
+    });
+}
+
+closeBtn.addEventListener("click", function() {
+    pop.classList.add("hide");
+});
+
+function message(output) {
+    pop.classList.remove("hide");
+    popMessage.innerText = output;
+}
+
+
